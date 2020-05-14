efbe964fdb838520d3308161690655a8569b8371
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2020-05-14 11:06:18 +0200

    correct mongodb setup

--- docker/db/config.js
+++
@@ -1,6 +0,0 @@
-db.auth('optimize', 'toor');
-
-db.collection.insert('config', {
-    "suggestionAPI": "​https://api.bing.com/osjson.aspx?lang=fr&q=​",
-    "suggestionLimit": 10
-});
---
+++ docker/db/init.js
@@ -0,0 +1,16 @@
+// https://docs.mongodb.com/manual/reference/method/db.createCollection/#db.createCollection
+
+db.createUser({
+    user: 'optimize',
+    pwd: 'toor',
+    roles: [
+        {
+            role: 'readWrite',
+            db: 'optimize'
+        }
+    ]
+});
+db.auth('optimize', 'toor');
+
+db.createCollection('config', { capped: true, max: 1, size: 100 });
+db.config.insert({ "suggestionAPI": "​https://api.bing.com/osjson.aspx?lang=fr&q=​", "suggestionLimit": 10 });
