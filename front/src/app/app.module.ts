b48df37aee98e732ad348466c540e116cf9f3212
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2020-05-14 15:32:25 +0200

    Scaffold component

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchBoxComponent } from './search-box/search-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchBoxComponent
   ],
   imports: [
     BrowserModule
