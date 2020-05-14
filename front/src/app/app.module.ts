c4c71742d3fdd388e872c0b867b12cde256b5d8f
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2020-05-14 21:06:40 +0200

    Add loader

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SearchBoxComponent } from './search-box/search-box.component';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SearchBoxComponent
+    SearchBoxComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule
--- front/src/app/search-box/search-box.component.ts
+++ front/src/app/search-box/search-box.component.ts
@@ -9,6 +9,7 @@ import axios from 'axios'
 export class SearchBoxComponent implements OnInit {
 
   public results = [];
+  public isLoading = false;
 
   constructor() { }
 
@@ -16,13 +17,15 @@ export class SearchBoxComponent implements OnInit {
   }
 
   onChange(event) {
+    this.isLoading = true;
 
     const params = new URLSearchParams;
     params.append('q', event.target.value);
 
     axios.get(`http://api.lvh.me/suggestions?${params.toString()}`)
         .then(res => this.results = res.data.map(d => d.keyword))
-        .catch(e => console.error(e));
+        .catch(e => console.error(e))
+        .finally(() => this.isLoading = false);
   }
 
 }
